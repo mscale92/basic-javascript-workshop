1bb3ad2f2cbd41bd32dbb350939e50731f71dae0
Mary Scale <scalemary@gmail.com>
2016-10-05 02:33:44 +0000

    Added another function

--- answers.js
+++ answers.js
@@ -1,8 +1,17 @@
 //yay a comment!
 
-var firstchar = function firstch(string){
-    var stringArray = string.split();
-    return stringArray;
+//Return the first character of a string.
+var firstchar = function(str){
+    return str.charAt(0);
 };
 
 firstchar("hello");
+firstchar("world");
+firstchar("");
+
+//Return the last character of a string.
+var lastChar = function(str){
+    return str.charAt(str.length - 1)
+};
+
+lastChar("world");
