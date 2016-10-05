f0841199bdff81c81b456c4a642c8ac27484c2a7
Mary Scale <scalemary@gmail.com>
2016-10-05 02:45:54 +0000

    Added two more functions

--- answers.js
+++ answers.js
@@ -9,6 +9,22 @@ firstchar("hello");
 firstchar("world");
 firstchar("");
 
+
+//Return the last character of a string.
+var lastChar = function(str){
+    return str.charAt(str.length - 1)
+};
+
+lastChar("world");
+
+
+//Return the last character of a string.
+var lastChar = function(str){
+    return str.charAt(str.length - 1)
+};
+
+lastChar("world");
+
 //Return the last character of a string.
 var lastChar = function(str){
     return str.charAt(str.length - 1)
